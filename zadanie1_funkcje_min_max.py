d6928d32898e9bd23faa5c8a3301becab4734fa5
jpalmowswroc <42138312+jpalmowswroc@users.noreply.github.com>
2018-09-17 16:57:13 +0200

    Add files via upload

---
+++ zadanie1_funkcje_min_max.py
@@ -0,0 +1,9 @@
+def maxymalna(*args):
+    return max(*args)
+def minimalna(*args):
+    return min(*args)
+
+zmienna_max = maxymalna(5,10,11)
+print(zmienna_max)
+zmienna_min = minimalna(5,10,11)
+print(zmienna_min)
